50d27ee8b333b9299c73fa18447929de6c472985
ashunium <voidash14@gmail.com>
2025-12-15 13:12:11 +0530

    Create page.js

---
+++ app/tournaments/page.js
@@ -0,0 +1,13 @@
+export default function Tournaments() {
+  return (
+    <main style={{ padding: "60px" }}>
+      <h1>Tournaments</h1>
+
+      <div style={{ marginTop: "30px" }}>
+        <h2>🏆 Champions League</h2>
+        <p>16 teams • Group + Knockout</p>
+        <a href="/tournaments/champions-league">View</a>
+      </div>
+    </main>
+  );
+}
