58806fb59e4d7c12544d3e5980fe24ad2d7cb79d
ashunium <voidash14@gmail.com>
2025-12-15 13:20:21 +0530

    Add header with navigation links to layout

--- app/layout.js
+++ app/layout.js
@@ -7,8 +7,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body style={{ margin: 0, background: "#0b0b0b", color: "white", fontFamily: "Arial" }}>
-        {children}
-      </body>
+  <header style={{
+    padding: "20px 40px",
+    borderBottom: "1px solid #222"
+  }}>
+    <a href="/" style={{ marginRight: "20px" }}>Home</a>
+    <a href="/tournaments">Tournaments</a>
+  </header>
+
+  {children}
+</body>
     </html>
   );
 }
